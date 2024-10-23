7f8e4193eb7340615eb85e9c742acc905cbe77eb
Manuel1977 <fernandezestebanmanuel9@gmail.com>
2024-10-23 20:44:14 +0200

    busqueda con filtro

--- src/app/clientes/busqueda-clientes/busqueda-clientes.component.ts
+++ src/app/clientes/busqueda-clientes/busqueda-clientes.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { map, Observable } from 'rxjs';
+import { ClienteEntity } from '../../../domain/entities/cliente.entity';
+import { ClienteService } from '../../../services/cliente.service';
 
 @Component({
   selector: 'app-busqueda-clientes',
   standalone: true,
   imports: [
-    CommonModule,
+    CommonModule, MatTableModule, MatFormFieldModule, MatInputModule,
   ],
   templateUrl: './busqueda-clientes.component.html',
   styles: `
@@ -15,4 +21,29 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BusquedaClientesComponent { }
+export class BusquedaClientesComponent implements AfterViewInit  {
+
+  displayedColumns: string[] = ['nombre', 'telefonoPrincipal', 'tipoCliente', 'horario'];
+
+  clienteService = inject(ClienteService);
+  dataSource: any;
+
+  constructor() {
+    this.clienteService.getAllClientes().subscribe(res => {
+      this.dataSource = new MatTableDataSource(res)
+    }
+    )
+  }
+  ngAfterViewInit(): void {
+    this.clienteService.getAllClientes().subscribe(res => {
+      this.dataSource = new MatTableDataSource(res)
+    }
+    )
+  }
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+}
